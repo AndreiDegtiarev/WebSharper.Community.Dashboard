708eeab87bef7489e0fe45c3d66f882001b77008
jaipriyadarshicode <jaipriyadarshicode>
2017-09-19 01:34:14 -0400

    font family, font size and width properties added

--- WebSharper.Community.Dashboard/Widgets/TextBoxWidget.fs
+++ WebSharper.Community.Dashboard/Widgets/TextBoxWidget.fs
@@ -6,26 +6,38 @@ open WebSharper.UI.Next.Client
 open WebSharper.UI.Next.Html
 open WebSharper.Community.Dashboard
 
+
 [<JavaScript>]
 type TextBoxWidget =
-  {
-      TextBoxWidgetData:WorkerData
-  }
-  static member Create = {TextBoxWidgetData = WorkerData.Create "Text" [InPortData.CreateString "in Text" "txt"] []} 
-  static member FromWorker = (fun (worker:Worker) -> {TextBoxWidgetData = worker.ToData}) 
-  interface IWorkerData with
-    override x.Data = x.TextBoxWidgetData
-    override x.Run = None
-    override x.Render  = Some(fun worker -> 
+    {
+        TextBoxWidgetData:WorkerData
+    }
+    static member Create = {TextBoxWidgetData = WorkerData.Create "Text" [InPortData.CreateString "in Text" "txt"
+                                                                          InPortData.CreateNumber "Width" 50.0
+                                                                          InPortData.CreateNumber "Font-Size" 25.0
+                                                                          InPortData.CreateSelect "Font-Family" (0, ["Courier New" ; "Segoe UI"; "Freestyle Script"])]
+                                                                          []} 
+    static member FromWorker = (fun (worker:Worker) -> {TextBoxWidgetData = worker.ToData}) 
+    interface IWorkerData with
+        override x.Data = x.TextBoxWidgetData
+        override x.Run = None
+        override x.Render  = Some(fun worker ->
                             let strView = worker.InPorts.[0].PortValue.View 
-                                          |> View.Map (fun msg -> 
-                                                         match msg.Value with
-                                                         |MessageBus.Number(value) -> ((int)value).ToString()
-                                                         |MessageBus.String(value) -> value
-                                                         |MessageBus.Boolean(value) -> value.ToString()
-                                                         |_ -> "Wrong port value format"
-                                                      )
-                            divAttr [Attr.Class "bigvalue"] [
-                                             textView strView
-                            ] :> Doc)
+                                            |> View.Map (fun msg -> 
+                                                            match msg.Value with
+                                                            |MessageBus.Number(value) -> ((int)value).ToString()
+                                                            |MessageBus.String(value) -> value
+                                                            |MessageBus.Boolean(value) -> value.ToString()
+                                                            |_ -> "Wrong port value format"
+                                                        )
+                            let widthView = worker.InPorts.[1].PortValue.View |> View.Map (fun msg -> msg.Value.AsNumber.ToString() + "px")
+                            let fontSizeView = worker.InPorts.[2].PortValue.View |> View.Map (fun msg -> msg.Value.AsNumber.ToString() + "px")          
+                            let fontFamilyView = worker.InPorts.[3].PortValue.View |> View.Map (fun msg -> let (ind,lst) = msg.Value.AsSelect
+                                                                                                           lst.[ind])
+                            divAttr [Attr.Class "bigvalue"
+                                     Attr.DynamicStyle "width" widthView
+                                     Attr.DynamicStyle "font-size" fontSizeView
+                                     Attr.DynamicStyle "font-family" fontFamilyView
+                                    ]
+                                    [textView strView] :> Doc)
 

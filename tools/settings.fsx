9a07608e061e61aa027a87d29e57f0ec3b54f8af
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2018-04-05 22:57:57 +0200

    package info updated

--- tools/settings.fsx
+++ tools/settings.fsx
@@ -3,7 +3,9 @@ module Settings
     #load @"..\..\WebSharper.Community.Build\gloabalSettings.fsx"
     let projectName = "Dashboard"
     let projectDescription = "WebSharper floating panel component"
-    let dependencies = [("WebSharper.Charting","")
+    let dependencies = [("WebSharper.UI","")
+                        ("WebSharper.FSharp","")
+                        ("WebSharper.Charting","")
                         ("WebSharper.Data","")
                         ("WebSharper.Community.Panel",globalSettings.version)]
     let releaseNotes = "Database event, behavior adjusted"
